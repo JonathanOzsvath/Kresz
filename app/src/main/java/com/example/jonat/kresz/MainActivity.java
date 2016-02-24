bf48bea3458474bd1b1fc05b2b88e791be29cdfd
JonathanOzsvath <ozsvathjonathan@gmail.com>
2016-02-24 15:43:19 +0100

    timer

--- app/src/main/java/com/example/jonat/kresz/MainActivity.java
+++ app/src/main/java/com/example/jonat/kresz/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jonat.kresz;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,20 +26,23 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
 
     private List<ListViewItem> mItems;
     private ImageView imageView;
     private ListView listView;
-    private ListViewAdapter listViewAdapter;
-    private TextView feladat;
+    public ListViewAdapter listViewAdapter;
+    private TextView feladat, textViewTime;
     private JSONObject obj;
     private int szam;
     private int temakor;
     int helyes;
     int aktfeladat, aktpontszam;
     private TextView feladatSzam, pontSzam;
+    private CounterClass timer;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +64,8 @@ public class MainActivity extends Activity {
             temakor = 0;
             aktfeladat = 1;
             aktpontszam = 0;
+            timer = new CounterClass(60000,1000);
+            timer.start();
 
             try {
                 Random r = new Random();
@@ -73,9 +80,10 @@ public class MainActivity extends Activity {
 
         findView();
 
-        //feladat(703449);
+        //feladat(703479);
         feladat(szam);
 
+        progressBar.setMax(60);
         feladatSzam.setText(aktfeladat + "/55");
         pontSzam.setText(aktpontszam + "/75");
 
@@ -105,6 +113,7 @@ public class MainActivity extends Activity {
                 }
 
                 listView.setEnabled(false);
+                timer.cancel();
             }
         });
     }
@@ -132,6 +141,10 @@ public class MainActivity extends Activity {
                 aktfeladat++;
                 feladatSzam.setText(aktfeladat + "/55");
                 listView.setEnabled(true);
+
+                timer.cancel();
+                timer.start();
+                textViewTime.setTextColor(Color.parseColor("#fffb64"));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -220,6 +233,8 @@ public class MainActivity extends Activity {
         feladat = (TextView) findViewById(R.id.feladat);
         feladatSzam = (TextView) findViewById(R.id.feladatszam);
         pontSzam = (TextView) findViewById(R.id.pontszam);
+        textViewTime = (TextView) findViewById(R.id.textViewTime);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
     }
 
     @Override
@@ -229,5 +244,38 @@ public class MainActivity extends Activity {
         outState.putInt("temakor", temakor);
         outState.putInt("aktFeladat", aktfeladat);
         outState.putInt("aktPontszam", aktpontszam);
+    }
+
+
+    public class CounterClass extends CountDownTimer {
+
+        /**
+         * @param millisInFuture    The number of millis in the future from the call
+         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+         *                          is called.
+         * @param countDownInterval The interval along the way to receive
+         *                          {@link #onTick(long)} callbacks.
+         */
+        public CounterClass(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            long millis = millisUntilFinished;
+            String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
+                    TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                    TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+
+            System.out.println(hms);
+            textViewTime.setText(hms);
+            progressBar.setProgress((int) TimeUnit.MILLISECONDS.toSeconds(millis));
+        }
+
+        @Override
+        public void onFinish() {
+            textViewTime.setText("Az idő lejárt.");
+            textViewTime.setTextColor(Color.RED);
+        }
     }
 }
